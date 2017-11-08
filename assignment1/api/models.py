583145f59602b8262e7e0c24fa7707b2349664dc
Saurabh <saurabhbansod1992@gmail.com>
2017-11-08 17:03:34 +0530

    Basis setup for api user view

--- assignment1/api/models.py
+++ assignment1/api/models.py
@@ -1,1 +1,46 @@
 from django.db import models
+
+class Venue(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    venue_id = models.ForeignKey(Venue, related_name='items')
+
+
+class Space(models.Model):
+    hour_price = models.FloatField()
+    item_id = models.ForeignKey(Item, related_name='spaces')
+
+
+class Product(models.Model):
+    price = models.FloatField()
+    item_id = models.ForeignKey(Item, related_name='products')
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    registered = models.IntegerField(default=0)
+
+
+class Booker(models.Model):
+    created = models.IntegerField(default=0)
+    user_id = models.ForeignKey(User, related_name='bookers')
+
+
+class Booking(models.Model):
+    created = models.IntegerField(default=0)
+    booker_id = models.ForeignKey(Booker, related_name='bookings')
+
+
+class BookingItem(models.Model):
+    quantity = models.IntegerField()
+    locked_piece_price = models.FloatField()
+    locked_total_price = models.FloatField()
+    start_timestamp = models.IntegerField(default=0)
+    end_timestamp = models.IntegerField(default=0)
+    booking_id = models.ForeignKey(Booking, related_name='booking_items')
+    item_id = models.ForeignKey(Item, related_name='booking_items')
--- assignment1/api/views.py
+++ assignment1/api/views.py
@@ -1,6 +1,50 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from api.models import User
+from api.serializers import ApiSerializer
 
-from django.shortcuts import render
+@csrf_exempt
+def user_list(request):
+    """
+    List all code users, or create a new user.
+    """
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = ApiSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
-# Create your views here.
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ApiSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def user_detail(request, pk):
+    """
+    Retrieve, update or delete a code user.
+    """
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ApiSerializer(user)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ApiSerializer(user, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return HttpResponse(status=204)
